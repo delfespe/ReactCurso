0b6e3bf28c8ca9c5abebe224a5126d2c05ef30bc
delfin <delfinespejod@gmail.com>
2020-06-14 13:50:36 -0500

    Hola adios mundo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import HolaMundo, { AdiosMundo } from "./components/HolaMundo";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <HolaMundo></HolaMundo>
+        <AdiosMundo></AdiosMundo>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HolaMundo.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function HolaMundo() {
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+      <h2>Delfin Espejo</h2>
+    </div>
+  );
+}
+
+export function AdiosMundo() {
+  return (
+    <div>
+      <h1>Adios Mundo</h1>
+    </div>
+  );
+}

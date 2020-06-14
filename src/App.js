7f41e139b691f278a67019461bce13be2f7a387d
delfin <delfinespejod@gmail.com>
2020-06-14 14:34:16 -0500

    Saludar: Objeto y Funcion enviado como parametros

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import HolaMundo, { AdiosMundo } from "./components/HolaMundo";
+import Saludar from "./components/Saludar";
 
 function App() {
+  //objeto que se pasa a componente
+  const user = {
+    nombre: "Delfin Espejo",
+    edad: 27,
+    color: "Azul",
+  };
+  //funcion que se pasa a un componente
+  const saludarFn = (nombre, edad) => {
+    //console.log("Hola " + nombre + " tiene " + edad + " años.");
+    //template string
+    console.log(`Hola ${nombre} tiene ${edad} años.`);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <HolaMundo></HolaMundo>
-        <AdiosMundo></AdiosMundo>
+        <Saludar userInfo={user} saludarFn={saludarFn} />
       </header>
     </div>
   );

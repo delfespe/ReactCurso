8589ef8f0a992f3e74fd00ea11b9ceaa8b3b1fec
delfin <delfinespejod@gmail.com>
2020-06-14 16:22:15 -0500

    Hook de estado: si variable se actualiza, componente se actualiza

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Saludar from "./components/Saludar";
 
 function App() {
-  //objeto que se pasa a componente
-  const user = {
-    nombre: "Delfin Espejo",
-    edad: 27,
-    color: "Azul",
-  };
-  //funcion que se pasa a un componente
-  const saludarFn = (nombre, edad) => {
-    //console.log("Hola " + nombre + " tiene " + edad + " años.");
-    //template string
-    console.log(`Hola ${nombre} tiene ${edad} años.`);
+  //estado
+  const [stateCar, setStateCar] = useState(false);
+  //hook state
+  const encenderApagar = () => {
+    console.log("Encender/Apagar");
+    //setStateCar(!stateCar);
+    //pedir el estado a la funcion setStateCar
+    setStateCar((prevValue) => !prevValue);
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Saludar userInfo={user} saludarFn={saludarFn} />
+        <h3>El auto esta {stateCar ? "Encendido" : "Apagado"}</h3>
+        <button onClick={encenderApagar}>Encender/Apagar</button>
       </header>
     </div>
   );
